85381d472b9ffb524d333e84552e8f55ff4e4faf
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-24 08:54:48 +0000

    import ex3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,22 @@
 # Your solution to Exercise 3
 
+n = int(input("Input a number: "))
+colour =""
+
+if n == 0:
+    print("Green")
+elif n < 10:
+    colour = "Red" if n % 2 == 1 else "Black"
+elif n < 18:
+    colour = "Black" if n % 2 == 1 else "Red"
+elif n < 28:
+    colour = "Red" if n % 2 == 1 else "Black"
+elif n < 36:
+    colour = "Black" if n % 2 == 1 else "Red"
+else:
+    colour = "The bet will not play!"
+
+print(colour)
+
+
+
