e3c607e1e1dfea04eb00a80537f057a48db10260
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-06-05 13:49:07 +0000

    import ex8

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,17 @@
 # Your solution to Exercise 8
 
+n = int(input("Input a 3 digit number: "))
+target_number = int(input("What digit do you want to find?: "))
+
+n1 = n // 100
+n2 = (n // 10) % 10
+n3 = n % 10
+
+if n1 == target_number:
+    print("True")
+elif n2 == target_number:
+    print("True")
+elif n3 == target_number:
+    print("True")
+else:
+    print("False")
