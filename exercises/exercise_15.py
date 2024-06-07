2f4ad3c0faab0d1a0f1c2ece7fe3979bf2a6e053
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-06-07 08:07:49 +0000

    import ex5, 15 and test5

--- exercises/exercise_15.py
+++ exercises/exercise_15.py
@@ -1,2 +1,5 @@
 # Your solution to Exercise 15
 
+day = int(input("Input day: "))
+month = int(input("Input month: "))
+year = int(input("Input year: "))
--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,8 +1,12 @@
 # Your solution to Exercise 5
 
-a = float(input("Enter coefficient a: "))
-b = float(input("Enter coefficient b: "))
-c = float(input("Enter coefficient c: "))
+# a = float(input("Enter coefficient a: "))
+# b = float(input("Enter coefficient b: "))
+# c = float(input("Enter coefficient c: "))
+
+a = float(input())
+b = float(input())
+c = float(input())
 
 d = b**2 - (4*a*c)
 
