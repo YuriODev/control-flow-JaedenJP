2aa27e41f31bf27afed4fc5e23c515056299342e
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-06-07 08:00:34 +0000

    import ex14

--- exercises/exercise_14.py
+++ exercises/exercise_14.py
@@ -1,2 +1,13 @@
 # Your solution to Exercise 14
 
+n = int(input("Input 4-digit number: "))
+
+n1 = n // 1000
+n2 = (n // 100) % 10
+n3 = (n // 10) % 10
+n4 = n % 10
+
+if n1 == n4 and n2 == n3:
+    print("True")
+else:
+    print("False")
